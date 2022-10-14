bd894f64f14b2ebebc31b69aa1d61bd3165911dd
fathom820 <michaelfrank2002@gmail.com>
2022-10-14 09:01:33 -0400

    intellij

--- Main.java
+++ Main.java
@@ -3,11 +3,9 @@
  */
 class Main {
   public static void main(String[] args) {
-    System.out.println("Test");
     ClientServer clientServer = new ClientServer();
-    System.out.println("Test");
-    clientServer.setupServer();
-    System.out.println("Test");
 
+    clientServer.setupServer();
+    clientServer.setupClient();
   }
 }
